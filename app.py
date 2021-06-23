afe891cce08abb2ec70b943487d29f1c5f19c20b
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-06-23 16:27:15 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import json
+import requests
+
+
+def main():
+
+	st.title('this is it')
+	html_string = "this is"
+
+	st.markdown(html_string, unsafe_allow_html=True)
+	res=requests.get('http://ipinfo.io/')
+	data=res.json()
+	print(data)
+	st.write(data)
+
+
+
+if __name__ == "__main__":
+    main()
