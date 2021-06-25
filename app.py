96eaa7bd5dbcca0aa5a0c0c5193cd2fdbcf6fc2c
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-06-26 00:17:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,73 @@
-import streamlit as st
-import json
-import requests
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to get your coordinates.</p>
+
+<button onclick="getLocation()">Try It</button>
+
+<p id="demo"></p>
+
+<script>
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+</script>
 
 
 
-def main():
-
-	st.title('this is it')
-	html_string = "this is"
-
-	st.markdown(html_string, unsafe_allow_html=True)
-	res=requests.get('http://ipinfo.io/')
-	data=res.json()
-	print(data)
-	st.write(data)
 
 
 
-if __name__ == "__main__":
-    main()
+
+
+
+<form action="https://sheetdb.io/api/v1/tzligcj8zu6x4" method="POST" id="sheetdb-form">
+  Name: <input name="data[Latitude]">
+  Email: <input name="data[Longitude]">
+  <button type="submit">Submit</button>
+</form>
+
+
+
+
+
+
+
+
+
+
+<script>
+  var form = document.getElementById('sheetdb-form');
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      alert('success')
+    });
+  });
+</script>
+
+
+
+
+
+</body>
+</html>
+
